1963b02f110a3bb6cb832ecac6bbcb1042a0462e
‘khalahm253’ <khalahm253@gmail.com>
2018-03-11 15:32:00 -0700

    first question test passes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
-  
+
 "The sum of 4 and 7 is 11."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
@@ -32,11 +32,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var productOfTwoNumbers = a * b;
+  var proString = 'The product of ' + a + ' and ' + b + 'is' + productOfTwoNumbers + '.';
+  return [productOfTwoNumbers, proString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
